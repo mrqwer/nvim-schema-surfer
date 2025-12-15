67d460ed341421d1a25fc669e3addb6bc5cfbeee
mrqwer <qlzpam45@gmail.com>
2025-12-16 00:40:38 +0500

    fix: surfer correct lua api

--- plugin/surfer.lua
+++ plugin/surfer.lua
@@ -1,6 +1,13 @@
-if exists("g:loaded_schema_surfer") then return end
-g:loaded_schema_surfer = 1
+-- plugin/surfer.lua
+
+if vim.g.loaded_schema_surfer == 1 then
+  return
+end
+vim.g.loaded_schema_surfer = 1
 
 vim.api.nvim_create_user_command("SchemaSurf", function(opts)
-    require("schema-surfer").load_schema(opts.args)
-end, { nargs = 1, desc = "Connect to DB: :SchemaSurf postgres://..." })
+  require("schema-surfer").load_schema(opts.args)
+end, {
+  nargs = 1,
+  desc = "Connect to DB: :SchemaSurf postgres://..."
+})
